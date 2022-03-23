3d2e5ca9dbabcaa8ec3278a137b07fd00a870de4
nar2p1nk <farrisisqandar@gmail.com>
2022-03-23 10:48:21 +0800

    maybe i've done it, still need some work tho, now all endpoints need a token

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const passport = require('passport');
 const bodyParser = require('body-parser');
-
+const jwt = require('jsonwebtoken');
 const userC = require('./model');
-
+const express_jwt = require('express-jwt');
 require('./auth');
 
 const routes = require('./routes');
@@ -16,13 +16,37 @@ app.use(bodyParser.json())
 
 app.use('/',routes)
 
+app.use(express_jwt({secret:'gila',algorithms:['HS256']}))
+
+app.get('/protected',
+    authenticatejwt,
+    (req,res)=>{
+        res.json({
+            status:200,
+            user:'verified',
+            welcome:req.user.user.username
+        })
+    }
+)
+
+
 app.use('/user',
-    passport.authenticate('jwt',{session:false}),
+//    authenticatejwt,
     secureRoutes);
 
 app.get('/',(req,res)=>{
-    res.json({server:'Ok'})
+    res.json({server:req.query.token})
 })
+
+function authenticatejwt(req,res,next){
+        const user = userC.findUserById(req.user.user.id)
+        if(!user){
+            console.log('no user found')
+            res.sendStatus(401)
+        }
+            next();
+        }
+
 
 app.use((err,req,res,next)=>{
     res.status(err.status || 500);
